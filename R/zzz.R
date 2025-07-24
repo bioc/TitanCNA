eed2321f669bed90ebc750c8f1baefb6de4ab07b
lshep <lori.shepherd@roswellpark.org>
2025-07-24 08:31:07 -0400

    Deprecated
    - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
 .onLoad <- function(lib, pkg) {
     library.dynam("TitanCNA", pkg, lib)
 } 
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.23")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
